e3bb33ffc5cf062900d9579bc84cd3a1ead4af2c
Minh <tranhoangminh1707@gmail.com>
2021-05-18 08:14:17 +0900

    2_mikroORM-type-graphql-crud

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('Testing 1 2 3')
+import "reflect-metadata";
+import { PostResolver } from "./resolvers/post";
+import { HelloResolver } from "./resolvers/hello";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+const main = async () => {
+	const orm = await MikroORM.init(microConfig);
+	await orm.getMigrator().up();
+
+	const app = express();
+
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver],
+			validate: false,
+		}),
+
+		context: () => ({
+			em: orm.em,
+		}),
+	});
+
+	apolloServer.applyMiddleware({ app });
+
+	app.listen(4000, () => {
+		console.log("Server started on localhost:4000");
+	});
+};
+
+main().catch((err) => {
+	console.error(err);
+});
+
+console.log("Testing 1 2 3");
